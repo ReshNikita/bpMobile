f5f0bfd4f7855d6d68d5a09b0cd3757a99380434
ReshNikita <rieshietnikov.nikita.1998@mail.ru>
2024-07-20 21:12:11 +0300

    added function for adaptive layout in Portuguese

--- main.js
+++ main.js
@@ -141,4 +141,20 @@ const changeStylesFR = () => {
 };
 changeStylesFR();
 
+const changeStylesPT = () => {
+  if (width === 414 && height === 896 && langFromUrl === "pt") {
+    mainHeading.style.margin = "105px 59px 40.7px";
+  }
+  if (width === 390 && height === 844 && langFromUrl === "pt") {
+    mainHeading.style.margin = "65px 39px 40.7px";
+  }
+  if (width === 375 && height === 812 && langFromUrl === "pt") {
+    mainHeading.style.margin = "65px 39px 40.7px";
+  }
+  if (width === 375 && height === 667 && langFromUrl === "pt") {
+    mainHeading.style.margin = "20px 39px 26.7px";
+  }
+};
+changeStylesPT();
+
 window.addEventListener("resize", () => window.location.reload());
